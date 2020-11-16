097180784eb5f06a4218d15bd5d1f7117e070252
Ilya Doronin <ilyaroadar@gmail.com>
2020-11-17 01:22:43 +0300

    add translate function

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import os
 from dotenv import load_dotenv
 from telethon import TelegramClient, events
 
+from translate import translate_word
+
 load_dotenv()
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
@@ -14,17 +16,21 @@ bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 @bot.on(events.NewMessage(pattern='/start'))
 async def start(event):
     """Send a message when the command /start is issued."""
-    await event.respond('Привет! Я бот-попугай, повторяю твои сообщения)')
+    await event.respond('Привет! Я бот-переводчик - введи слово и я его переведу)')
     raise events.StopPropagation
 
 @bot.on(events.NewMessage)
 async def echo(event):
-    """Echo the user message."""
-    await event.respond(event.text)
+    
+    translated_word = translate_word(event.text)
+
+    await event.respond(translated_word)
+
 
 def main():
     """Start the bot."""
     bot.run_until_disconnected()
+
 
 if __name__ == '__main__':
     main()
---
+++ translate.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+from dotenv import load_dotenv
+import requests
+
+load_dotenv()
+
+TOKEN = os.getenv('YANDEX_TOKEN')
+FOLDER_ID = os.getenv('YANDEX_FOLDER_ID')
+
+
+def translate_word(word):
+
+    url = 'https://translate.api.cloud.yandex.net/translate/v2/translate'
+    headers = {'Content-Type': 'application/json', 'Authorization': f'Api-Key {TOKEN}'}
+    payload = {'folder_id': FOLDER_ID, 'texts': [word], 'targetLanguageCode': 'ru'}
+
+    response = requests.post(url, json=payload, headers=headers)
+
+    resp_dict = json.loads(response.text)
+
+    return resp_dict['translations'][0]['text']
+
+
+word = 'think'
+
+print(translate_word(word))
